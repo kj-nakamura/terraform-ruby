7f75821ba779df4c54c8984e5898c4dbe9c9f34c
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2020-05-18 08:53:44 +0900

    create todo content

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'top#index'
+  resources :todos
   resources :health_check, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
